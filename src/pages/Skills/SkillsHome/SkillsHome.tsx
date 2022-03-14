449f4f1c22bed6dc7c0c673c45ea78eadb5e63ca
Felipe Lira <felipelira1908@hotmail.com>
2022-03-14 13:34:03 -0300

    feat(skills): adding soft skills

--- src/pages/Skills/SkillsHome/SkillsHome.tsx
+++ src/pages/Skills/SkillsHome/SkillsHome.tsx
@@ -2,6 +2,11 @@ import "./SkillsHome.scss";
 
 import { Button } from "../../../components/global/Button";
 
+import lamp from "../../../assets/lamp.svg";
+import ruler from "../../../assets/ruler.svg";
+import timer from "../../../assets/timer.svg";
+
+
 const SkillsHome = () => {
   return (
     <div className="skillsHome">
@@ -13,6 +18,21 @@ const SkillsHome = () => {
         I'm passionate about writing good and consistent code. I believe that products have the power to positively impact people's lives.
         <br /><br />
         My prefer front-end tools are <b>React</b> and <b>Vue.js</b>.
+      </div>
+
+      <div className="softSkills">
+        <div className="softSkill">
+          <img className="softSkillImg" src={ruler} alt="Pixel-Perfect" />
+          <h3 className="softSkillText">Pixel-Perfect</h3>
+        </div>
+        <div className="softSkill">
+          <img className="softSkillImg" src={timer} alt="Fast" />
+          <h3 className="softSkillText">Fast</h3>
+        </div>
+        <div className="softSkill">
+          <img className="softSkillImg" src={lamp} alt="Intuitive" />
+          <h3 className="softSkillText">Intuitive</h3>
+        </div>
       </div>
 
       <div className="buttonsWrapper">
