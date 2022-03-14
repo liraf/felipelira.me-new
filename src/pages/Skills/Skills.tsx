5d4fd7cb99108faff439d97de09ebc760a8e8491
Felipe Lira <felipelira1908@hotmail.com>
2022-03-13 21:56:29 -0300

    feat(skills): setup skills page with title and description

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,6 +1,19 @@
+import "./Skills.scss";
+
 const Skills = () => {
   return (
     <div className="skills">
+      <div className="container">
+        <h1 className="title">My skills</h1>
+
+        <div className="description">
+          I’m <b>Felipe</b>, a 25 year old <b>front-end developer</b> from Brazil.
+          <br /><br />
+          I'm passionate about writing good and consistent code. I believe that products have the power to create a positive impact on people's lives.
+          <br /><br />
+          I love creating apps on Vue and React.
+        </div>
+      </div>
     </div>
   );
 }
